a8e99a0d8c8384d6cc39f439dbab42f0c7a2f52c
Hans Josephsen <hans@bukholm.com>
2025-05-10 15:41:25 +0200

    feat: initial release

--- release.config.js
+++ release.config.js
@@ -1,6 +1,8 @@
 /* eslint-disable no-template-curly-in-string */
 module.exports = {
   plugins: [
+    "@semantic-release/commit-analyzer",
+    "@semantic-release/release-notes-generator",
     [
       "@semantic-release/npm",
       {
